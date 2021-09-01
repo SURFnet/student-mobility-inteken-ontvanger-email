d89c3ea36768ec9a344f0bfee1f6025c31d52cb8
Okke Harsta <oharsta@zilverline.com>
2021-09-01 10:39:38 +0200

    Use language header example

--- src/main/java/mail/api/IntakeEndpoint.java
+++ src/main/java/mail/api/IntakeEndpoint.java
@@ -5,11 +5,13 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.mail.MessagingException;
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -25,7 +27,9 @@ public class IntakeEndpoint {
     }
 
     @PostMapping(value = "/intake", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Map<String, Object>> intake(@RequestBody Map<String, Map<String, Object>> body) throws MessagingException {
+    public ResponseEntity<Map<String, Object>> intake(HttpServletRequest request, @RequestBody Map<String, Map<String, Object>> body) throws MessagingException {
+        //Use the language to differentiate in error messages and / or any textual return information
+        String language = request.getHeader("Accept-Language");
         //See /test/resources/data/request.json for an example request-body
         Map<String, Object> person = body.get("person");
         Map<String, Object> offering = body.get("offering");
@@ -43,10 +47,12 @@ public class IntakeEndpoint {
                 (String) person.get("secondaryMail"),
                 (String) person.get("initials"));
 
+        String message = StringUtils.hasText(language) && language.startsWith("nl") ? "Controlleer je mailbox voor meer informatie" : "Check your mailbox for more information";
+
         Map<String, Object> result = new HashMap<>();
         result.put("result", "ok");
         result.put("code", 200);
-        result.put("message", "Check your mailbox for more information");
+        result.put("message", message);
         result.put("oo-api-offering-id", offeringId);
         //put a redirect if the user needs to be redirected to a different gui
 
